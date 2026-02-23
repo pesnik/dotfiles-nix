1477f9a450de1acbf43b6cf95a73593a03696cdb
pesnik <hasanrakibul.masum@gmail.com>
2026-02-23 06:58:34 +0600

    feat: add ghostty terminal

---
+++ home/programs/ghostty.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
+
+    settings = {
+      font-family = "JetBrainsMono Nerd Font";
+      font-size = 13;
+      background-opacity = 0.95;
+      window-padding-x = 8;
+      window-padding-y = 8;
+      theme = "catppuccin-mocha";
+    };
+  };
+}
