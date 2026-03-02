5f8b33e800a2715144fb79b5f5c74e949b1f0f55
Rakibul Hasan <hasanrakibul.masum@gmail.com>
2026-03-02 23:28:14 +0600

    Implement home-manager for PesnikOS
    Add home-manager configuration for PesnikOS.

--- flake.nix
+++ flake.nix
@@ -1,20 +1,16 @@
 {
   description = "pesnik's NixOS configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, home-manager, nixvim, ... }@inputs:
   let
     system = "x86_64-linux";
@@ -35,6 +31,14 @@
           }
         ];
       };
+    };
+
+    homeConfigurations."pesnik@PesnikOS" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = { inherit inputs; };
+      modules = [
+        ./home/hosts/PesnikOS.nix
+      ];
     };
   };
 }
